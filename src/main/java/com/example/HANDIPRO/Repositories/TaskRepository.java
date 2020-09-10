52457b7e4d670fb4ab3c84bc22f23f87ba40ad45
Seweryn <sewerynde@gmail.com>
2020-09-10 13:09:43 +0200

    Ready to merge with test (adding to database is working)

---
+++ src/main/java/com/example/HANDIPRO/Repositories/TaskRepository.java
@@ -0,0 +1,14 @@
+package com.example.HANDIPRO.Repositories;
+
+import com.example.HANDIPRO.models.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository {
+    List<Task> findAll();
+
+    Task save(Task Entity);
+
+    Optional<Task> findById(int id);
+}
