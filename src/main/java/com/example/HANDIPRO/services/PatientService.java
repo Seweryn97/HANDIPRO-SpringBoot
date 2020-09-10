6eca089f80959e375538fe95dd1bcaa56fd08aef
Seweryn <sewerynde@gmail.com>
2020-09-10 13:08:18 +0200

    Ready to merge with test (adding to database is working)

--- src/main/java/com/example/HANDIPRO/services/PatientService.java
+++ src/main/java/com/example/HANDIPRO/services/PatientService.java
@@ -10,21 +10,41 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class PatientDTOService {
+public class PatientService {
     private PatientRegistrationRepository repository;
 
-    PatientDTOService(final PatientRegistrationRepository repository){
+    public PatientService(final PatientRegistrationRepository repository){
         this.repository = repository;
     }
 
     public PatientUpdateDTO updatePatientWithPhysiotherapist (Patient patient,PatientUpdateDTO patientUpdateDTO){
+        if(repository.findById(patientUpdateDTO.getId()).isEmpty()){
+            throw new IllegalArgumentException("Patient is not found");
+        }
+
         patient.setPhysiotherapist(patientUpdateDTO.getPhysiotherapist());
         repository.save(patient);
+
         return new PatientUpdateDTO();
     }
 
     public PatientReadDTO readPatient(Patient patient){
+        if(repository.findAll().isEmpty()){
+            throw new IllegalArgumentException("Repository is empty");
+        }
+
         Patient result = repository.save(patient);
+
         return  new PatientReadDTO(result);
+    }
+
+    public boolean isPasswordFormatOk(Patient patient){
+
+        boolean hasAtLeastOneBigLetter = !patient.getPassword().equals(patient.getPassword().toLowerCase());
+        boolean hasAtLeastOneInt = patient.getPassword().matches(".*\\d.*");
+        boolean isLongerOrEqualThanEight = patient.getPassword().length() >= 8;
+
+        return hasAtLeastOneBigLetter && hasAtLeastOneInt && isLongerOrEqualThanEight;
+
     }
 }
